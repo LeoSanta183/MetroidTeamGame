3acc3e669c2c8d067ce3d87b4c2efb32422f1b54
LeoSanta183 <santazleo2004@gmail.com>
2024-02-10 17:08:35 -0500

    Added some things

---
+++ MetroidGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public GameObject bulletPrefab;
+    public GameObject bigBulletPrefab;
+    public float groundCheckDistance = 10f; // Distance to check for ground
+    public LayerMask groundLayer;
+    public float jumpHeight = 10f;
+    private Rigidbody2D rb;
+    private bool betterWeapon;
+    public float bulletSpeed = 10;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        betterWeapon = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shooting();
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+
+        if(horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(3.182355f, 3.182355f, 3.182355f);
+        }
+        else if(horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-3.182355f, 3.182355f, 3.182355f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+
+
+    }
+    void Shooting()
+    {
+        if (betterWeapon == false)
+        {
+            float direction = Mathf.Sign(transform.localScale.x);
+            Vector3 spawnPosition = transform.position + new Vector3(direction, 0f, 0f);
+            GameObject bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity);
+            Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+            bulletRigidbody.velocity = new Vector2(direction * bulletSpeed, 0f);
+        }
+        else if(betterWeapon == true)
+        {
+            float direction = Mathf.Sign(transform.localScale.x);
+            Vector3 spawnPosition = transform.position + new Vector3(direction, 0f, 0f);
+            GameObject bullet = Instantiate(bigBulletPrefab, spawnPosition, Quaternion.identity);
+            Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+            bulletRigidbody.velocity = new Vector2(direction * bulletSpeed, 0f);
+
+
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(collision.gameObject);
+            betterWeapon = true;
+        }
+    }
+}
